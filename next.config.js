3c2f6368a417909b44da92d34a008b4e8e97025b
Deepak Modi <118504803+decodewithdeepak@users.noreply.github.com>
2025-08-09 08:09:42 +0530

    Update Next.js configuration and TypeScript target version

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
